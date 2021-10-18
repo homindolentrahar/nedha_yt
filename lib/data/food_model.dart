c441a2a9d7290001fcc93b5fc86848629e5bb8a8
Rahardiyan Eko Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2021-10-18 23:12:30 +0700

    Add some stuff for detail screen

--- lib/data/food_model.dart
+++ lib/data/food_model.dart
@@ -4,6 +4,9 @@ class FoodModel {
   final String name;
   final String description;
   final double price;
+  final double rating;
+  final int calories;
+  final int cookingTime;
   final bool isFavorite;
   final int favoriteCount;
 
@@ -13,6 +16,9 @@ class FoodModel {
     required this.name,
     required this.description,
     required this.price,
+    required this.rating,
+    required this.calories,
+    required this.cookingTime,
     required this.isFavorite,
     required this.favoriteCount,
   });
